4cb31fcf304c29142a5be88b922e5e9e1c270c98
Moslem Ajra <moss.ajra.dev@gmail.com>
2021-06-19 22:01:51 +0100

    Update app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
 }
 
 export default App;
+
+/*
+  The properties that we get from Route which the match,
+  the history and the location property  we only get access to them
+  from the first component that gets passed to our Route
+*/
